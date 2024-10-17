e40aeed47089a2dcf3ca4a37e0bb56a981b9cf0a
Luis Pardo <luis.pardo@the-cocktail.com>
2024-10-17 15:54:11 +0200

    deleted imports from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MathModule } from './math/math.module';
-import { CatApiClient } from './cat_api/cat.api.client';
-import { CatsController } from './cat_api/cat.api.controller';
 import { CatApiModule } from './cat_api/cat.api.module';
 
 @Module({
   imports: [MathModule, CatApiModule],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatApiClient],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
